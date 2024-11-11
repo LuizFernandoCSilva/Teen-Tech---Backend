6f693995d67d5d24536ca74286d73f14b173b532
Luiz Fernando <d2022003915@unifei.edu.br>
2024-11-11 17:10:05 -0300

    Adicionando tratamento de excessões

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,26 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({ error: 'No token provided' });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Token not provided or malformed" });
   }
-  try{
-    const decoded = jwt.verify(token.replace('Bearer ',''), JWT_SECRET);
+
+  const token = authHeader.replace("Bearer ", "");
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
     next();
-
-  }catch(err){
-    return res.status(401).json({ error: 'Invalid token' });
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token expired" });
+    }
+    return res.status(401).json({ error: "Invalid token" });
   }
-}
+};
 
 export default authMiddleware;
-
--- routes/public.js
+++ routes/public.js
@@ -1,27 +1,72 @@
-import express from 'express';
-import bcrypt from 'bcrypt';
-import { PrismaClient } from '@prisma/client';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 const prisma = new PrismaClient();
-
 const JWT_SECRET = process.env.JWT_SECRET;
 
+if (!JWT_SECRET) {
+  console.error("JWT secret is missing");
+  process.exit(1);
+}
+
+// Cadastro de usuário
 // Cadastro de usuário (aluno ou professor)
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res, next) => {
   try {
     const { name, email, password, role, registrationNumber } = req.body;
 
-    if (!['student', 'teacher'].includes(role)) {
-      return res.status(400).json({ error: 'Invalid role' });
+    // Validação do número de matrícula para professores
+    const numberMatriculation = [
+      "2022010384",
+      "2022003933",
+      "2022002551",
+      "2022013072",
+      "2022003915",
+      "2022002186",
+      "2022003307",
+      "2022003334",
+    ];
+
+    if (
+      role === "teacher" &&
+      !numberMatriculation.includes(registrationNumber)
+    ) {
+      return res.status(400).json({ error: "Invalid registration number" });
     }
 
+    if (!["student", "teacher"].includes(role)) {
+      return res.status(400).json({ error: "Invalid role" });
+    }
+
+    // Verificar se o e-mail p/ estudante  já existe
+    const existingStudant = await prisma.student.findUnique({
+      where: { email },
+    });
+
+    if (existingStudant) {
+      return res.status(400).json({ error: "Email already in use" });
+    }
+
+    // Caso seja um professor, verificar se o e-mail já existe na tabela de professores
+    if (role === "teacher") {
+      const existingTeacher = await prisma.teacher.findUnique({
+        where: { email },
+      });
+
+      if (existingTeacher) {
+        return res.status(400).json({ error: "Email already in use" });
+      }
+    }
+
+    // Gerar hash da senha
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
     let userDB;
-    if (role === 'student') {
+    if (role === "student") {
       userDB = await prisma.student.create({
         data: {
           name,
@@ -33,9 +78,11 @@ router.post('/register', async (req, res) => {
           email: true,
         },
       });
-    } else if (role === 'teacher') {
+    } else if (role === "teacher") {
       if (!registrationNumber) {
-        return res.status(400).json({ error: 'Registration number is required for teachers' });
+        return res
+          .status(400)
+          .json({ error: "Registration number is required for teachers" });
       }
 
       userDB = await prisma.teacher.create({
@@ -43,7 +90,7 @@ router.post('/register', async (req, res) => {
           name,
           email,
           password: hashPassword,
-          registrationNumber
+          registrationNumber,
         },
         select: {
           name: true,
@@ -53,54 +100,55 @@ router.post('/register', async (req, res) => {
       });
     }
 
+    // Retorna o usuário criado com status 201
     res.status(201).json(userDB);
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    // Tratamento de erro caso o código de erro seja P2002 (erro de restrição única do Prisma)
+    if (err.code === "P2002") {
+      return res.status(400).json({ error: "Email already in use" });
+    }
+    next(err); // Passa o erro para o manipulador global de erros
   }
 });
 
 // Login de usuário
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Verificar se o usuário é estudante
     let user = await prisma.student.findUnique({
       where: { email },
     });
 
-    
     let role;
     if (user) {
-      role = 'student';
-    }// Se não for encontrado como estudante, verificar se é professor
-     else {
+      role = "student";
+    } else {
       user = await prisma.teacher.findUnique({
         where: { email },
       });
       if (user) {
-        role = 'teacher';
+        role = "teacher";
       }
     }
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid password or email' });
+      return res.status(400).json({ error: "Invalid password or email" });
     }
 
-    const token = jwt.sign(
-      { id: user.id, role },
-      JWT_SECRET,
-      { expiresIn: '2h' }
-    );
+    const token = jwt.sign({ id: user.id, role }, JWT_SECRET, {
+      expiresIn: "2h",
+    });
 
     res.status(200).json({ token });
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
